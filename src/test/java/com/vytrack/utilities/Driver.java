66778765326dc401f0c1458a73fc97383b696cbb
Denis Pupkov <d3664s@outlook.com>
2020-04-19 18:17:00 -0400

    cucumber April 19,2020 Sunday

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -1,22 +1,16 @@
 package com.vytrack.utilities;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 public class Driver {
-
     //same for everyone
     private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
-
     //so no one can create object of Driver class
     //everyone should call static getter method instead
     private Driver() {
-
     }
-
     /**synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
      *
      * Thread safety reduces performance but it makes everything safe.
@@ -31,14 +25,14 @@ public class Driver {
             String browser = ConfigurationReader.getProperty("browser").toLowerCase();
             switch (browser) {
                 case "chrome":
-                    WebDriverManager.chromedriver().version("79").setup();
+                    WebDriverManager.chromedriver().setup();
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.addArguments("--start-maximized");
                     driverPool.set(new ChromeDriver(chromeOptions));
                     break;
                 case "chromeheadless":
                     //to run chrome without interface (headless mode)
-                    WebDriverManager.chromedriver().version("79").setup();
+                    WebDriverManager.chromedriver().setup();
                     ChromeOptions options = new ChromeOptions();
                     options.setHeadless(true);
                     driverPool.set(new ChromeDriver(options));
@@ -53,7 +47,6 @@ public class Driver {
         }
         return driverPool.get();
     }
-
     /**synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
      *
      * Thread safety reduces performance but it makes everything safe.
@@ -89,7 +82,6 @@ public class Driver {
         }
         return driverPool.get();
     }
-
     public static void closeDriver() {
         if (driverPool != null) {
             driverPool.get().quit();
